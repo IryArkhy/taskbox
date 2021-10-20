7290aeea68db06140780dd87bf31429a4d7ee858
Iryna Arkhypchenko <37odri@gmail.com>
2021-10-20 22:20:53 +0300

    commit

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,49 @@
+import TaskList from "./TaskList";
+
+import { PureTaskList } from './TaskList';
+import * as TaskStories from "./Task.stories";
+
+export default {
+  component: PureTaskList,
+  title: 'Task List',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+}
+
+const Template = args => <PureTaskList {...args} />;
+const tasks = [
+  { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+  { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+  { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+  { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+  { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+  { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+]
+
+export const Default = Template.bind({});
+Default.args = {
+  tasks
+}
+
+const tasksPinned = [
+  ...Default.args.tasks.slice(0, 5),
+  { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+]
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  tasks: tasksPinned
+}
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
+};
